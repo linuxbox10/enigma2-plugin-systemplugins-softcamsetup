47128066ef30b13b586e95af0559270a8e186041
linuxbox10 <jaysmith940@hotmail.co.uk>
2017-06-30 12:30:38 +0100

    File upload

---
+++ usr/lib/enigma2/python/Plugins/SystemPlugins/SoftcamSetup/plugin.py
@@ -0,0 +1,20 @@
+# Embedded file name: /usr/lib/enigma2/python/Plugins/SystemPlugins/SoftcamSetup/plugin.py
+from . import _
+from Plugins.Plugin import PluginDescriptor
+
+def main(session, **kwargs):
+    import Sc
+    session.open(Sc.ScSelection)
+
+
+def menu(menuid, **kwargs):
+    if menuid == 'cam':
+        return [(_('Softcam Manager...'),
+          main,
+          'softcam_setup',
+          45)]
+    return []
+
+
+def Plugins(**kwargs):
+    return [PluginDescriptor(name='Softcam Cardserver Manager', description='Lets you configure your softcams', where=PluginDescriptor.WHERE_MENU, fnc=menu), PluginDescriptor(name='Softcam Cardserver Manager', description='start/stop Softcams', where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main)]
